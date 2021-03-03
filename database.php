e7a4ec80be8e60293d698da23a5618846521a3af
EatDrinkAndBeMiller <71196286+EatDrinkAndBeMiller@users.noreply.github.com>
2021-03-03 12:07:33 -0700

    database updates

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=todolist';
-    $username = 'root';
-    //$password = '';
+    $dsn = 'mysql:host=hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ug20x9abexpao2nr';
+    $username = 'ur2e3p7129us8h23';
+    $password = 'zw14qvu2f0t0u9ax';
 
     try {
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = 'Database Error: ';
         $error_message .= $e->getMessage();
